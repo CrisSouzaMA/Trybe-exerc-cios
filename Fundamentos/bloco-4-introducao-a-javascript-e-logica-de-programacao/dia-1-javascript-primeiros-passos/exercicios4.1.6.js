bc86625738f630cc5f5f3f4bf7ede92476e04432
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2021-08-26 18:56:53 -0300

    Dia 4 - exericio 6

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios4.1.6.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios4.1.6.js
@@ -1,22 +1,22 @@
-let chessPiece;
+let chessPiece = "BISPO";
 
-switch (chessPiece){
-    case "Rei":
+switch (chessPiece.toLowerCase ()){
+    case "rei":
         console.log("Só pode andar uuma casa por vez e não pode se movimentar para casas que estejam sendo atacadas");
         break;
-    case "Dama":
+    case "dama":
         console.log("Peça mais importante. Pode ir para a direção que quiser mas não pode pular casa");
         break;
-    case "Torre":
+    case "torre":
         console.log("POde se movimentar em qualquer direção mas não pode pular casas");
         break;
-    case "Bispo":
+    case "bispo":
         console.log("Só se movimenta em casas da mesma cor e não pode pular nenhuma peça");
         break;
-    case "Cavalo":
+    case "cavalo":
         console.log("Tem umm movimento especial que parece um L e pode pular casas");
         break;
-    case "Peão":
+    case "peão":
         console.log("O peão só se movimenta para frente");
         break;
     default:
