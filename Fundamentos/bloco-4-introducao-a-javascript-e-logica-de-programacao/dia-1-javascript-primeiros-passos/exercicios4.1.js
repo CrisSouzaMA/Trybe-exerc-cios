617ae1b47576f9c34edf152463203299b2641f18
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2021-08-26 15:37:02 -0300

    Dia 04 - exercicio 1

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios4.1.js
@@ -0,0 +1,12 @@
+let A = 5;
+let B = 8;
+
+console.log("Soma" ,A + B);
+
+console.log("Subtração" ,A - B);
+
+console.log("Multiplicação" ,A * B);
+
+console.log("Divisão" , A / B);
+
+console.log("Módulo" ,A % B);
