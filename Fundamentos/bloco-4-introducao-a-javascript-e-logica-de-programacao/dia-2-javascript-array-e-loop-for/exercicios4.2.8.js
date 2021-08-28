317cd4061eaf241e549282fa7070ecdae9348d7e
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2021-08-28 09:59:50 -0300

    Bloco 4 - Dia 2 - Exercicio 8

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios4.2.8.js
@@ -0,0 +1,8 @@
+//Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let sum = [];
+
+for(let index = 1; index <= 25; index += 1){
+    sum.push(index);
+}
+console.log(sum);
