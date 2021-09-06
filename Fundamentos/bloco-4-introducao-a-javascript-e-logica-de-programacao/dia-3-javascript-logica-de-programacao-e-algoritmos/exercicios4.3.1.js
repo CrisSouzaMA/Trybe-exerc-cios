1a532895280510eb1378ec316b6f6693c4cbe471
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2021-09-06 17:30:39 -0300

    Solucionando exercicio

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios4.3.1.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios4.3.1.js
@@ -1,15 +1,13 @@
 //Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 
 //imprima na tela um quadrado feito de asteriscos de lado de tamanho n . 
 
-let n = 4;
+let n = 5;
+let symbol = '*';
+let inputLine = '';
 
-let quadradoLinha = "*";
-let quadradoAltura = "";
-
-for (let index = 0; index < n; index += 1) {
-   quadradoAltura = quadradoAltura + quadradoLinha;
-
-    for(let index = 0; index < n; index += 1){
-        console.log(quadradoAltura);
-    }
-}
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+};
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  console.log(inputLine);
+};
