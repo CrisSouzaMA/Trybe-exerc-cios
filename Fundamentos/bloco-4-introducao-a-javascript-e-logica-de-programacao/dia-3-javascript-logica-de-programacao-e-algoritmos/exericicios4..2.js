e415756ee1ac6a5fbccf7b532f73f0dc3dd7039e
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2021-09-06 17:44:17 -0300

    Tentando resolver questao 2

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exericicios4..2.js
@@ -0,0 +1,19 @@
+//2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. 
+
+/*let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+};
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  console.log(inputLine);
+};*/
+
+let n = 5;
+let symbol = '*';
+let inputline = '';
+for (let lineIndex = 0; lindeIndex < n; lineIndex += 1){
+    if()
+}
