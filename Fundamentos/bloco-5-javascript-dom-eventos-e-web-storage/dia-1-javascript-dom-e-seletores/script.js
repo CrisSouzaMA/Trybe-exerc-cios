8017cb6d5e63267302ab57c2bec3ea9899d5ba86
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2021-09-08 18:46:41 -0300

    Fixaçao de GetElementsByTagName e GetElementsByClassName

---
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,14 @@
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+
+//1 - Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
+
+document.getElementById("page-title").innerText = "Estrelas além do tempo";
+
+//2- Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
+
+document.getElementById("second-paragraph").innerText = "O filme fala de mulheres negras na NASA na década de 60. Essas mulheres fizeram toda diferença na vida de outras mulheres"
+
+//3 -Por fim, recupere o subtítulo e altere-o também.
+
+document.getElementById("subtitle").innerText = "Sobre o filme:";
