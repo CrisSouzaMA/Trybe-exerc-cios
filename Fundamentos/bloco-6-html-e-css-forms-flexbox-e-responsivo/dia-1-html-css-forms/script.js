47002f4827b7cf1d5fa8ee6c679a2b7b6136a4e6
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2021-09-18 10:12:06 -0300

    Exercicios Bloco 6 - Dia 1

---
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,11 @@
+/*function countries (){
+  const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'];
+  const countrie = document.getElementById('countries');
+  const option = document.createElement('option');
+
+  for(let index = 0; index=estados.length; index +=1){
+    option.innerText = estados[index];
+    countrie.add(option);
+  }
+}
+countries()
