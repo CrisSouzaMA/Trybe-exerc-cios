ea105dad5da814a312cd715045275ec1e4c29216
Cristiane Souza <cris.souzamoreiraealmeida@gmail.com>
2021-09-20 20:43:17 -0300

    Segunda tentativa de resoluçao do exercicio

--- Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,11 +1,95 @@
-/*function countries (){
-  const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'];
+function countries (){
   const countrie = document.getElementById('countries');
-  const option = document.createElement('option');
-
-  for(let index = 0; index=estados.length; index +=1){
-    option.innerText = estados[index];
-    countrie.add(option);
+  const estados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+  
+  for(let index = 0; index < estados.length; index +=1){
+    const option = document.createElement('option');
+    countrie.appendChild(option).innerText = estados[index];
+    countrie.appendChild(option).value = estados[index];
   }
 }
 countries()
+
+let inputs {
+  nome: {
+    maxLength: 40;
+    required: true;
+  }
+  email: {
+    maxLength: 50;
+    required: true;
+  }
+  cpf: {
+    maxLength: 11;
+    required: true;
+  }
+  adress: {
+    maxLength: 200;
+    required: true;
+  }
+  city: {
+    maxLength: 28;
+    required: true;
+  }
+  countries: {
+    type: 'select';
+    required: true;
+  }
+  tipoDeREsidencia:{
+    type: 'radio';
+    required: true;
+  }
+  exp: {
+    maxLength: 1000;
+    required: true;
+  }
+  cargo1: {
+    maxLength: 40;
+    required: true;
+  }
+  descCargo1: {
+    maxLength: 500;
+    required: true;
+  }
+  dataInicio: {
+    type: 'date';
+    required: true;
+  }
+}
+
+function valuationName (input, name){
+
+    let trimmed = input.value.trim();
+    let validation = inputs[name];
+  
+    if(validation.required && trimmed.length === 0){
+      return false;
+    }
+  
+    if(validation.maxLength && trimmed.length > validation.maxLength){
+      return false;
+    }
+    
+    return true;
+  }
+}
+
+function valuationDate (input, name){
+
+  if(input.value.length === 0){
+    return {
+      message: "Data inválida"
+    }
+  }
+
+  const regex = /^\d\d\/\d\d\/\d\d\d\d$/; 
+}
+
+window.onload = function () {
+  countries();
+  let submitButton = document.querySelector('.submit-button');
+  submitButton.addEventListener('click', handleSubmit);
+
+  let clearButton = document.querySelector('.clear-button');
+  clearButton.addEventListener('click', clearFields);
+}
